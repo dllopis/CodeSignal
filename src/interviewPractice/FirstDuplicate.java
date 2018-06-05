c0b71763752a9a01b78d5244070d4712fc17ea23
Dennis LLopis <dllopis16@gmail.com>
2018-06-04 23:48:04 -0400

    FirstDuplicate challenge completed -- Google Interview Question --

--- src/interviewPractice/FirstDuplicate.java
+++ src/interviewPractice/FirstDuplicate.java
@@ -1,80 +1,61 @@
 package interviewPractice;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class FirstDuplicate {
-	// time out error with this attempt!
-	
-	
-	static int[] a = {8, 4, 6, 2, 6, 4, 7, 9, 5, 8};
-	static int[] b = {2,3,3};
-	static int firstDuplicate(int[] a)
-	{
-		 // this will hold index of duplicates found and returned when loop is done
-    	int firstDup = -1;
-    
-    // this will hold index of duplicate to compare with first dup
-		int checkDup = 0;
-    
-    // this will be the pointer to check and see if a duplicate exist for this integer a[j]
-		int j = 0;
+
+	public static int firstDuplicate(int[] a) {
 		
-    
-		for(int i = 1;i<a.length;i++)
-		{
-            
-			if(a[j] == a[i])
-			{
-                // first duplicate that is found will initialize firstDup
-                // and continue to start finding additional dupes to compare indexes
-				if(firstDup == -1)
-				{
-                    // index of duplicate is saved
-					firstDup = i;
-                    
-                    // increment pointer to next element to check for dups
-					j++;
-                    
-                    // reset i to position after j and continue checks
-					i=j+1;
-                    
-                    // jump to next iteration since we found our firstDup
-					continue;
-				}
-				
-                
-                // saved index of our currentDup
-				checkDup = i;
-				
-                // check if it's less than our currentDup
-				if(checkDup < firstDup)
-				{
-                    // if it is, then save our new index of the duplicate
-					firstDup = checkDup;
-                    // increment our pointer
-					j++;
-                    // and reset i to one position after j
-					i = j+1;
-				}
-			}
-            
-            // this ensures everything is checked for duplicates
-            // when j is equal to a.length, we can exit the loop and return the result
-            if(j != a.length-1 && i == a.length-1)
-			{
-				j++;
-				i=j;
-			}
-		}
-    
-    // cycled through array and no duplicate existed
-        if(firstDup == -1)
-			return -1;
-    
-	return a[firstDup];
+		int firstDuplicate = -1; // -1 means a duplicate has not been found yet
+		int indexDuplicate = -1; // serves as index location for first duplicate
+		
+		// Hash set is used to check for duplicates - unique values can only be stored -- no duplicates
+		// stores original array contents to help search faster for duplicates
+		Set<Integer> keys = new HashSet<Integer>();
+		
+		// iterate through original array
+        for(int i = 0;i<a.length;i++){
+        	
+        	// if false occurs, then we've found a dupe
+            if(keys.add(a[i]) == false)
+            {
+            	// save the current index in order to compare for which duplicate occurred first
+            	int currIndex = i;
+            	
+            	// first duplicate found -- store value and index to compare future duplicates
+            	if(firstDuplicate == -1)
+            	{
+            		firstDuplicate = a[currIndex];
+            		indexDuplicate = currIndex;
+            		System.out.println("Duplicate at index: " + i + "\tValue: " + a[i]);
+            		continue;
+            	}else{
+            		System.out.println("Duplicate at index: " + i + "\tValue: " + a[i]);
+            		System.out.println("current index: " + currIndex + "\tFirstDuplicate index: " +indexDuplicate);
+            		
+            		// checks to see which duplicate occured first and assigns to firstDuplicate
+            		if(currIndex < indexDuplicate)
+            		{
+            			firstDuplicate = a[currIndex];
+            			System.out.println("Earlier duplicate found -- replace --");
+            		}
+            	}
+            }
+        }
+
+        if(firstDuplicate == -1)
+        {
+        	System.out.println("No duplicates!");
+        }else
+            System.out.println("FirstDuplicate: " + firstDuplicate);
+        
+		return firstDuplicate;
 	}
-	
 	public static void main(String args[])
 	{
-		System.out.println("First Dupe: " + firstDuplicate(a));
-		System.out.println("First Dupe: " + firstDuplicate(b));
+		
+		int[] a = {2, 1, 3, 5, 3, 2};
+		firstDuplicate(a);
 	}
 }
